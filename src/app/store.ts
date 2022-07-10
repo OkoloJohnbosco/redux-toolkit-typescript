77bdc29e27598612f48e565d35aae308c5c18799
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2022-07-10 21:53:56 +0100

    added rtk query

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/couter-slice";
+import { apiSlice } from "../features/dogs/dogs-api-slice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
---
+++ src/features/dogs/dogs-api-slice.tsx
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const DOGS_API_KEY = "2c6dd62f-1c34-4735-b41b-3d4973764a9e";
+
+interface Breed {
+  id: string;
+  name: string;
+  image: { url: string };
+}
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.thedogapi.com/v1",
+    prepareHeaders(headers) {
+      headers.set("x-api-key", DOGS_API_KEY);
+      return headers;
+    },
+  }),
+  endpoints(builder) {
+    return {
+      fetchBreeds: builder.query<Breed[], number | void>({
+        query(limit = 10) {
+          return `/breeds?limit=${limit}`;
+        },
+      }),
+    };
+  },
+});
